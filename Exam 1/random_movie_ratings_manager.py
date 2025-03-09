ae82f36ca3c9eb10298fa4ed39a545fd784842d4
tejuZAP <tkolawole@lewis.edu>
2025-03-09 10:27:57 -0500

    Makes more changes to exam 1

--- Exam 1/random_movie_ratings_manager.py
+++ Exam 1/random_movie_ratings_manager.py
@@ -5,13 +5,13 @@ rating = {}
 
 def addMovie(movieName) :
     #Asks for the users to add a movie title and adds it into the list called movieName
-    title = input("Enter movie name: ")
-    movieName[title] = []
+    title = input("Enter movie name: ") #Defines the input given by the users as title
+    movieName[title] = [] #places title into the movieName list
     
 def addRating(movieName, rating) :
     #Has the users select a movie in the list and assign a rating to it that is stored into the dictionary called rating
-    movieName = input("Select a movie to rate: ")
-    if movieName in movieName:
+    movieName = input("Select a movie to rate: ") #defines the input given by the users as movieName
+    if movieName in movieName: #if the input given by the users matches what is in the movieName list it ask the user to given a rating if not it prints an error
         while True:
             try:
                 rating = int(input("Enter rating(1-5 stars): "))
@@ -25,7 +25,7 @@ def addRating(movieName, rating) :
                 print(f"Movie is not in the manager")
 
 def generateRandomRating(movieName) :
-    #Adds a random rating to the movies in the list that gets stored to the dictionary
+    #Adds a random rating to the movies in the list that gets stored to the dictionary 
     for title in movieName:
         movieName[title].append(random.randint(1, 5))
 
@@ -44,11 +44,11 @@ while True:
     choice = input("Enter choice: ")
     
     if choice == "1":
-        addMovie(movieName)
+        addMovie(movieName) #calls the addMovie function
     elif choice == "2":
-        addRating(movieName, rating)
+        addRating(movieName, rating) #calls the addRating function
     elif choice == "3":
-        displayMovieRatings()
+        displayMovieRatings() #calls the displayMovieRatings function
     elif choice == "4":
         break
     else: 
